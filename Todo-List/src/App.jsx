46b905441f47cd34675057689ffc096812029c00
ShravaniMishra <shravniguddi@gmail.com>
2023-03-06 23:33:06 +0530

    Todo App created

---
+++ Todo-List/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  const[todo,setTodo]=useState("")
+  const[todos,setTodos]=useState([])
+
+  const handleChange = (event) => {
+    setTodo(event.target.value)
+  }
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    setTodos([...todos,todo])
+  }
+
+  return (
+    <>
+     <div className="App">
+      <div className="inner_box">
+        <h1>Todo List</h1>
+        <br />
+        <input type="text" placeholder='Add an Item' onChange={handleChange}/>
+        <button onClick={handleClick}> +👍</button>
+      </div>
+      <ol>
+        {/* <li>{todo}</li> */}
+        {
+          todos.map((elem)=>{
+            return  <li>{elem}</li>
+          })
+        }
+      </ol>
+      </div>
+    </>
+   
+  )
+}
+
+export default App
