8e9b2db6b3d7f9a3eff0f4249f80f6ff2d90e6c5
ShravaniMishra <shravniguddi@gmail.com>
2023-03-07 19:41:21 +0530

    Created Alert box and worked with  short styling by bootsrap

---
+++ UsingBootstrap/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h1>My Boostrap styling</h1>
+      <button className="btn btn-success" onClick={()=>alert("Welcome Here")}>Click Me</button>
+    </div>
+  );
+}
+
+export default App
