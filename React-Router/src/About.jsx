a9fb2c6b575024d3e71841f20fec4b1a699f4e8d
ShravaniMishra <shravniguddi@gmail.com>
2023-03-15 23:37:35 +0530

    used useRef

--- React-Router/src/About.jsx
+++ React-Router/src/About.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
 import { Box, Text } from "@chakra-ui/react";
 
 const About = () => {
+  const luckyName = useRef();
+  const [show, setShow] = useState(false);
+
+  const ChangeBy = (e) => {
+    e.preventDefault();
+    const name = luckyName.current.value
+    name === "" ? alert("Please enter your name") : setShow(true);
+  
+  };
+
   return (
     <>
+      <Box>
+        <form action="" onSubmit={ChangeBy}>
+          <label htmlFor="">Enter Your Lucky Name</label>
+          <input type="text" placeholder="Name" ref={luckyName} />
+          <br />
+          <button>Submit</button>
+        </form>
+        <Text>
+          {show ? `Your Lucky Name is ${luckyName.current.value}` : ""}
+        </Text>
+      </Box>
+
       <Box>
         <Text>
           My name is Shravani Mishra and{" "}
@@ -19,6 +41,6 @@ const About = () => {
       </Box>
     </>
   );
-}
+};
 
-export default About
+export default About;
